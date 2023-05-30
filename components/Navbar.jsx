541af7be1709a9cd03379d6bbef09f6bae043042
nikhil-223 <113900755+nikhil-223@users.noreply.github.com>
2023-05-30 10:21:05 +0530

    create-next-app@latest

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+const Navbar = () => {
+	return <nav className="">
+        <div>
+            <Image fill src=''>
+
+            </Image>
+        </div>
+        <div></div>
+    </nav>;
+};
+
+export default Navbar;
