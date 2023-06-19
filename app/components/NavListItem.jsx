b1187a211f210fa04a915653833b848470dde258
nikhil-223 <113900755+nikhil-223@users.noreply.github.com>
2023-06-19 17:30:45 +0530

    Update NavListItem.jsx

--- app/components/NavListItem.jsx
+++ app/components/NavListItem.jsx
@@ -7,13 +7,13 @@ const NavListItem = ({ href, label, isActive, icon:Icon }) => {
 		<>
 			<li
 				className={`md:flex sm:hidden hover:opacity-80 ${
-					isActive && "text-green-600"
+					isActive && "text-cyan-700"
 				}`}>
 				<a href={href}> {label}</a>
 			</li>
 			<li
 				className={`sm:flex md:hidden flex-col items-center ${
-					isActive && "text-green-600"
+					isActive && "text-cyan-700"
 				}`}>
 				<span className="p-2 text-xl hover:opacity-80">
 					<a href={href}>
